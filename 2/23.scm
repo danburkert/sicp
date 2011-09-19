1cd40dc3ca7c7c4e423af262ec724c4e14109ee4
Dan Burkert <danburkert@gmail.com>
2011-09-19 15:05:50 -0400

    fix for-each exercise after meeting

--- 2/23.scm
+++ 2/23.scm
@@ -1,14 +1,8 @@
-; the challenge here is figuring out how to get the side effect of
-; calling (proc (car items))
-
-; TODO: talk to professor Weimer about best way to implement the side
-; effect, or how to call two functions in a row, not using the return
-; of one.
-
 (define (for-each proc items)
-  (if (or (proc (car items))
-          (not (null? items)))
-    (for-each proc (cdr items))))
+  (if (not (null? items))
+    ((lambda (x) (for-each proc (cdr items))) (proc (car items))))))
+
+; test
 
 (for-each (lambda (x) (newline) (display x))
           (list 57 321 88))
